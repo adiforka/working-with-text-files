8f237a0e8987eedb31e7135fb5f5668298bf75a8
adi forkasiewicz <adiforka@gmail.com>
2020-05-02 14:55:13 +0200

    updates to try-with-resources

--- src/working_with_text_files/LineCombiner.java
+++ src/working_with_text_files/LineCombiner.java
@@ -6,38 +6,38 @@ import java.io.*;
 // method, and then write the combined lines into a target file (with write())
 public class LineCombiner implements TextProcessor {
 
+    //updated to try-with-resources
     public String read(File... files) throws IOException {
 
-        BufferedReader br = new BufferedReader(new FileReader(files[0]));
-        BufferedReader br2 = new BufferedReader(new FileReader(files[1]));
         StringBuilder sb = new StringBuilder("{\n");
 
-        String line;
-        String line2;
-        while ((line = br.readLine()) != null && (line2 = br2.readLine()) != null) {
+        try (BufferedReader br = new BufferedReader(new FileReader(files[0]));
+             BufferedReader br2 = new BufferedReader(new FileReader(files[1]))) {
 
-            sb.append(line);
-            sb.append(" ");
-            sb.append(line2);
-            sb.append("\n");
+            String line;
+            String line2;
+            while ((line = br.readLine()) != null && (line2 = br2.readLine()) != null) {
+
+                sb.append(line);
+                sb.append(" ");
+                sb.append(line2);
+                sb.append("\n");
+            }
         }
 
         sb.append("}");
-        br.close();
         return sb.toString();
     }
 
     public void write(String inputForTargetFile, File... files) throws IOException {
 
-        //append the target file instead of overwriting it
-        BufferedWriter bw = new BufferedWriter(new FileWriter(files[0], true));
-        bw.write(inputForTargetFile);
+        //changed to try-with-resources, which, if it fails, will throw an IOException up
+        //the call stack, but now there's no way we'll forget to close the reader and keep
+        //the file safe
 
-        //this is old-school, I know, and normally we'd use try-with-resources
-        //an AutoCloseable, but I wanted to keep this code cleaner and let the
-        //caller handle the exceptions wherever they run the program
-        //thing is: the writer won't actually write right into the file unless I
-        //call close on it after it writes
-        bw.close();
+        //append the target file instead of overwriting it
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(files[0], true))) {
+            bw.write(inputForTargetFile);
+        }
     }
 }
